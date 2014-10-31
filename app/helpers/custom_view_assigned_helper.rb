8e16a9bdd80ead82d5c58ee5a11460fc30706664
Alexander Bocharov <alex.bocharov@outlook.com>
2014-10-31 14:12:32 +0300

    Fixed the selection conditions of workflow rule

--- app/helpers/custom_view_assigned_helper.rb
+++ app/helpers/custom_view_assigned_helper.rb
@@ -1,6 +1,6 @@
 module CustomViewAssignedHelper
   def assignable_users(issue)
-    workflow_rules = WorkflowRule.where('old_status_id = ?', issue.status_id).group(:role_id).pluck(:role_id)
+    workflow_rules = WorkflowRule.where('new_status_id = ?', issue.status_id).group(:role_id).pluck(:role_id)
     target_members = Member.select { |member| MemberRole.select { |role| workflow_rules.include?(role.role_id) }
     .map(&:member_id).include?(member.id) }.map(&:user_id).sort
 
