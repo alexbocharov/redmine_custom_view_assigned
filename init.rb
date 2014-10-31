e8f8dc6b82f76502180ad9007c15350b24f0d2e0
Alexander Bocharov <alex.bocharov@outlook.com>
2014-10-31 09:12:32 +0300

    Initial commit

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :redmine_custom_view_assigned do
+  name 'Redmine Custom View Assigned plugin'
+  author 'Alexander Bocharov'
+  description 'This plugin for Redmine adds a custom view of the field assigned'
+  version '0.0.1'
+end
