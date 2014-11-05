9ac359bf8fe8bfef73986e3fe9302e7ab40b6a3e
Alexander Bocharov <alex.bocharov@outlook.com>
2014-11-05 08:53:12 +0300

    Fixing bug with determining valid members in project

--- app/helpers/custom_view_assigned_helper.rb
+++ app/helpers/custom_view_assigned_helper.rb
@@ -1,10 +1,11 @@
 module CustomViewAssignedHelper
   def assignable_users(issue)
+    current_project = Project.find(issue.project_id)
+
     workflow_rules = WorkflowRule.where('new_status_id = ?', issue.status_id).group(:role_id).pluck(:role_id)
     target_roles = MemberRole.select { |role| workflow_rules.include?(role.role_id) }.map(&:member_id).sort
-    target_members = Member.select { |member| target_roles.include?(member.id) }.map(&:user_id).sort
-
-    current_project = Project.find(issue.project_id)
+    target_members = Member.select { |member| target_roles.include?(member.id) &&
+        member.project_id == current_project.id }.map(&:user_id).sort
 
     types = ['User']
     types << 'Group' if Setting.issue_group_assignment?
