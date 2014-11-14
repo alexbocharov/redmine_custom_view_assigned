3177ab6b4394bea99fe22d311f525f9628e96d2b
Alexander Bocharov <alex.bocharov@outlook.com>
2014-11-14 17:13:21 +0300

    Added setting the group by. Rename 'label_no_group'->'label_custom_view_assigned_grouping_no_group'.

--- lib/redmine_custom_view_assigned.rb
+++ lib/redmine_custom_view_assigned.rb
@@ -3,5 +3,9 @@ module RedmineCustomViewAssigned
     def filtering_users
       Setting.plugin_redmine_custom_view_assigned['filtering_users']
     end
+
+    def grouping_mode
+      Setting.plugin_redmine_custom_view_assigned['grouping_mode']
+    end
   end
 end
